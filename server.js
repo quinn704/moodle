026b88cd5a9018ce0456749121381c353a6a4941
Andric Quinn Baticos <asbaticos@up.edu.ph>
2018-09-20 13:49:04 +0800

    added express application

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 'use strict'
 
+const express = require('express');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/SOCIAL_MEDIA', () => {
-	console.log('Connected...');
+	console.log('Database Connected...');
 });
+
+// Initialize server
+const app = express();
+
+// Initialize body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 // Declare model
 const PostSchema = new mongoose.Schema({
@@ -18,9 +27,20 @@ mongoose.model('Post', PostSchema);
 // Get mongoose model
 const Post = mongoose.model('Post');
 
-// Fetch data from collection
-Post.find((err, posts) => {
-	if (!err) {
-		console.log(posts);
-	}
+// Routes
+app.get('/', (request, response) => {
+	response.send('Welcome!');
+})
+
+app.get('/find-all-posts', (request, response) => {
+	// Fetch data from collection
+	Post.find((err, posts) => {
+		if (!err) {
+			response.send(posts);
+		}
+	});
 });
+
+app.listen(3000, ()=>{
+	console.log('Server started');
+})
