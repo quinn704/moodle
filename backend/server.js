63808c6da4b5f5a770102b85093f06082a4ba483
Andric Quinn Baticos <asbaticos@up.edu.ph>
2018-09-27 15:51:37 +0800

    added require(body-parser)

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@
 *                                                                                                 *
 **************************************************************************************************/
 'use strict';
+const body_parser = require('body-parser');             // middleware used for parsing the body of the request
 const express = require('express');                     // web framework for node
 const port = process.env.PORT || 3000;                  // port used to run the program
 const router = require(__dirname + '/routes/router');   // methods available for the application
@@ -38,6 +39,10 @@ start = () => {
   //  setting the environment for express
   app.set('case sensitive routing', true);
   app.disable('x-powered-by');
+
+  //  middleware packages
+  app.use(body_parser.urlencoded({extended: true}));
+  app.use(body_parser.json());
 
   router(app);
   return app.listen(port, () => {
