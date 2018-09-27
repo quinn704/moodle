8ffbeb8a75f6a56d38f76f4655f7a79e06918bcf
Andric Quinn Baticos <asbaticos@up.edu.ph>
2018-09-27 14:49:44 +0800

    done: schemas

--- backend/database/models.js
+++ backend/database/models.js
@@ -20,57 +20,57 @@
 'use strict';
 const mongoose  = require('mongoose');
 
-//  -- Class Schema
-var Class = mongoose.model('Class', new mongoose.Schema({
-  classId: mongoose.Schema.Types.ObjectId,
-  title: 'string',
-  section: 'string',
-  teacherId: mongoose.Schema.Types.ObjectId
-}))
-
-//  -- Inbox Schema
-var Inbox = mongoose.model('Inbox', new mongoose.Schema({
-  inboxId: mongoose.Schema.Types.ObjectId,
-  userId: mongoose.Schema.Types.ObjectId
-}))
-
-//  -- Message Schema
-var Message = mongoose.model('Message', new mongoose.Schema({
-  messageId: mongoose.Schema.Types.ObjectId,
-  timestamp : { type : Date, default: Date.now },
-  content: 'string',
-  senderId: mongoose.Schema.Types.ObjectId,
-  inboxId: mongoose.Schema.Types.ObjectId
-}))
-
-//  -- Post Schema
-var Post = mongoose.model('Post', new mongoose.Schema({
-  postId: mongoose.Schema.Types.ObjectId,
-  content: 'string',
-  timestamp : { type : Date, default: Date.now },
-  authorId: mongoose.Schema.Types.ObjectId,
-  classId: mongoose.Schema.Types.ObjectId,
-}))
-
-//  -- Student Schema
-var Student = mongoose.model('Student', new mongoose.Schema({
-  studentId: mongoose.Schema.Types.ObjectId,
-  userId: mongoose.Schema.Types.ObjectId
-}))
-
-//  -- Teacher Schema
-var Teacher = mongoose.model('Teacher', new mongoose.Schema({
-  teacherId: mongoose.Schema.Types.ObjectId,
-  userId: mongoose.Schema.Types.ObjectId
-}))
-
 //  -- User Schema
-var User = mongoose.model('User', new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   userId: mongoose.Schema.Types.ObjectId,
   name: 'string',
   password: 'string',
   email: 'string'
-}));
+}); var User = mongoose.model('User', UserSchema);
+
+//  -- Student Schema
+const StudentSchema = new mongoose.Schema({
+  studentId: mongoose.Schema.Types.ObjectId,
+  userId: UserSchema
+}); var Student = mongoose.model('Student', StudentSchema);
+
+//  -- Teacher Schema
+const TeacherSchema = new mongoose.Schema({
+  teacherId: mongoose.Schema.Types.ObjectId,
+  userId: UserSchema
+}); var Teacher = mongoose.model('Teacher', TeacherSchema);
+
+//  -- Class Schema
+const ClassSchema = new mongoose.Schema({
+  classId: mongoose.Schema.Types.ObjectId,
+  title: 'string',
+  section: 'string',
+  teacherId: TeacherSchema
+}); var Class = mongoose.model('Class', ClassSchema);
+
+//  -- Inbox Schema
+const InboxSchema = new mongoose.Schema({
+  inboxId: mongoose.Schema.Types.ObjectId,
+  userId: UserSchema
+}); var Inbox = mongoose.model('Inbox', InboxSchema);
+
+//  -- Message Schema
+const MessageSchema = new mongoose.Schema({
+  messageId: mongoose.Schema.Types.ObjectId,
+  timestamp : { type : Date, default: Date.now },
+  content: 'string',
+  senderId: UserSchema,
+  inboxId: mongoose.Schema.Types.ObjectId
+}); var Message = mongoose.model('Message', MessageSchema);
+
+//  -- Post Schema
+const PostSchema = new mongoose.Schema({
+  postId: mongoose.Schema.Types.ObjectId,
+  content: 'string',
+  timestamp : { type : Date, default: Date.now },
+  authorId: UserSchema,
+  classId: ClassSchema
+}); var Post = mongoose.model('Post', PostSchema);
 
 
 
