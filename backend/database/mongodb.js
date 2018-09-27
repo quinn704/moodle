c202100e65d9d891b3b4b38661d1884740e9ad74
Andric Quinn Baticos <asbaticos@up.edu.ph>
2018-09-27 13:53:09 +0800

    initialized database connection file

---
+++ backend/database/mongodb.js
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+*                                                                                                 *
+*     Computer Science 100 Exercise: Moodle                                                       *
+*                                                                                                 *
+*     PURPOSE                                                                                     *
+*                                                                                                 *
+*     USAGE                                                                                       *
+*                                                                                                 *
+*     SUBROUTINES AND FUNCTION SUBPROGRAMS REQUIRED                                               *
+*                                                                                                 *
+*     METHOD                                                                                      *
+*                                                                                                 *
+*     PROGRAMMER                                                                                  *
+*     Andric Quinn Baticos                                                                        *
+*                                                                                                 *
+*     DATE-WRITTEN                                                                                *
+*     September 2018                                                                              *
+*                                                                                                 *
+**************************************************************************************************/
+'use strict';
+const mongoose  = require('mongoose');
