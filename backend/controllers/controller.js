b15889652abc259994701202e5d1a54b751439ac
Andric Quinn Baticos <asbaticos@up.edu.ph>
2018-09-27 13:52:45 +0800

    initialized routes and controllers

---
+++ backend/controllers/controller.js
@@ -0,0 +1,32 @@
+/**************************************************************************************************
+*                                                                                                 *
+*     Computer Science 100 Exercise: Moodle                                                       *
+*                                                                                                 *
+*     PURPOSE                                                                                     *
+*                                                                                                 *
+*     USAGE                                                                                       *
+*                                                                                                 *
+*     SUBROUTINES AND FUNCTION SUBPROGRAMS REQUIRED                                               *
+*                                                                                                 *
+*     METHOD                                                                                      *
+*                                                                                                 *
+*     PROGRAMMER                                                                                  *
+*     Andric Quinn Baticos                                                                        *
+*                                                                                                 *
+*     DATE-WRITTEN                                                                                *
+*     September 2018                                                                              *
+*                                                                                                 *
+**************************************************************************************************/
+'use strict';
+const db = require(__dirname + "/../database/mongodb");
+
+
+
+
+
+
+
+
+
+
+//	-- END
---
+++ backend/routes/router.js
@@ -0,0 +1,36 @@
+/**************************************************************************************************
+*                                                                                                 *
+*     Computer Science 100 Exercise: Moodle                                                       *
+*                                                                                                 *
+*     PURPOSE                                                                                     *
+*                                                                                                 *
+*     USAGE                                                                                       *
+*                                                                                                 *
+*     SUBROUTINES AND FUNCTION SUBPROGRAMS REQUIRED                                               *
+*                                                                                                 *
+*     METHOD                                                                                      *
+*                                                                                                 *
+*     PROGRAMMER                                                                                  *
+*     Andric Quinn Baticos                                                                        *
+*                                                                                                 *
+*     DATE-WRITTEN                                                                                *
+*     September 2018                                                                              *
+*                                                                                                 *
+**************************************************************************************************/
+'use strict';
+const controller = require(__dirname + '/../controllers/controller');
+
+module.exports = (app) => {
+
+}
+
+
+
+
+
+
+
+
+
+
+//	-- END
